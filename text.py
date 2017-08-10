6fd3ba7d938b4cef42afa27c636d934ecd77ccde
yuyi_7 <962919526@qq.com>
2017-08-10 15:40:46 +0800

    chage size into iphone5

--- text.py
+++ text.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import re
+'第 0005 题： 你有一个目录，装了很多照片，把它们的尺寸变成都不大于 iPhone5 分辨率的大小。'
 
-#在一个文件中查找特定的单词
-n=0
-word='you'
+import os
+from PIL import Image
+iw = 1136
+ih = 640
 
-with open(r'C:\Users\Administrator\test.txt','r') as f:
-
-	for lines in f.readlines():
-		
-		if re.findall(word,lines.lower()):
-			n = n+1
-print('有%d个%s'%(n,word))
+def main():
+	path = input('please input the path of images: ')
 
 
-with open(r'C:\Users\Administrator\test.txt','r') as f:
-	n=0
-	files = f.read()
-	words = files.split()
-
-	n = len(words)
+	for root, dirs, files in os.walk(path):
+		for f in files :
+			if f.lower().endswith('jpg'):
+				images=Image.open(path+'/'+f)
+				target = chagesize(images)
+				target.save(path+'/'+'iphone5_'+f)
+        
+        
+def chagesize(images):
 	
-print('有%d个单词'%n)
-print(words)
-
-with open(r'C:\Users\Administrator\test.txt','r') as f:
-	data = f.read()
-	lis = data.split()
-	dic={}
-	for x in re.findall('[a-zA-z]+',lis):
-		if x not in dic:
-			dic[x] = 1
-		else:
-			dic[x] += 1
-print(dic)
+	w,h = images.size
+	if w>iw:
+	   	h=iw * h//w
+	   	w=iw
+	elif h>640:
+		  w = ih * w//h
+		  h=ih
+	target = images.resize((w,h),Image.ANTIALIAS)
+	return target
 	
+if __name__=='__main__':
+	main()
