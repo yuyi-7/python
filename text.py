b9a98640ae91a8b5aa86bdf0788f93f6cc40f75d
yuyi_7 <962919526@qq.com>
2017-08-10 17:33:11 +0800

    get the most word in exery files

--- text.py
+++ text.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-'第 0005 题： 你有一个目录，装了很多照片，把它们的尺寸变成都不大于 iPhone5 分辨率的大小。'
+'第 0006 题： 你有一个目录，放了你一个月的日记，都是 txt，为了避免分词的问题，假设内容都是英文，请统计出你认为每篇日记最重要的词。'
 
 import os
-from PIL import Image
-iw = 1136
-ih = 640
+import re
+from mypython import dic
 
-def main():
-	path = input('please input the path of images: ')
+filetxt=[]
+def getfile():
+	path=input('please input path: ')
+	for root,dirs,files in os.walk(path):
+		for f in files:
+			if f.lower().endswith('.txt'):
+				filetxt.append(os.path.join(root,f))
+				
 
-
-	for root, dirs, files in os.walk(path):
-		for f in files :
-			if f.lower().endswith('jpg'):
-				images=Image.open(path+'/'+f)
-				target = chagesize(images)
-				target.save(path+'/'+'iphone5_'+f)
-        
-        
-def chagesize(images):
-	
-	w,h = images.size
-	if w>iw:
-	   	h=iw * h//w
-	   	w=iw
-	elif h>640:
-		  w = ih * w//h
-		  h=ih
-	target = images.resize((w,h),Image.ANTIALIAS)
-	return target
-	
+def findword():
+	word={}
+	for i in range(len(filetxt)):
+		with open(filetxt[i]) as f:
+			words = f.read()
+			word_dic = re.findall(r'[a-z]+',words.lower())
+		
+			for w in word_dic:
+				if w not in word:
+					word[w] = 1
+				elif w in word:
+					word[w] += 1
+			s=dic.dict2list(word)
+			sorte = sorted(s,key=lambda x:x[1],reverse = True)
+			print('在%s文件中，单词%s出现了%s次\n'%(os.path.abspath(filetxt[i]),sorte[0][0],sorte[0][1]))
+			
+		
 if __name__=='__main__':
-	main()
+	getfile()
+	findword()
