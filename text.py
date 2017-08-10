ccd6c63458961bd78452e5baee700bee0fa6ab9f
yuyi_7 <962919526@qq.com>
2017-08-11 06:11:56 +0800

    make a random word picture

--- text.py
+++ text.py
@@ -1,38 +1,28 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-'第 0007 题： 有个目录，里面是你自己写过的程序，统计一下你写过多少行代码。包括空行和注释，但是要分别列出来。'
+'第 0010 题： 使用 Python 生成类似于下图中的字母验证码图片'
 
-import os
-import re
+from PIL import Image,ImageDraw,ImageFont,ImageFilter
+import random
 
-tool=[]
-def getfiles():
-	path=input('please input the path: ')
-	for root,dirs,files in os.walk(path):
-		for f in files:
-			if f.lower().endswith('.py'):
-				
-				tool.append(root+'/'+f)
-			
-def cul():
-	n=r=z=0
-	for i in range(len(tool)):
-		with open(tool[i],'r',encoding='utf-8') as f:
-			for lines in f.readline():
-				n = n + 1
-				
-				
-				if lines.strip() == '':
-						r = r + 1
-				
-				if re.match(r'^#',lines.strip()):
-					z += 1
-			
-					
-	print('你总共写了%d行代码，%d个空行，%d行注释'%(n,r,z))
-	
-if __name__=='__main__':
-	getfiles()
-	cul()
+w=60*4
+h=60
+
+image = Image.new('RGB',(w,h))
+
+font = ImageFont.truetype('C:\Windows\Fonts\Arial.ttf', 36)
+
+draw = ImageDraw.Draw(image)
+
+for x in range(w):
+	for y in range(h):
+		draw.point((x,y),fill = (random.randint(64,225),random.randint(64,225),random.randint(64,225)))
+
+for i in range(4):
+	draw.text((i*60+10,10),chr(random.randint(65,90)),font = font,fill = (random.randint(32,125),random.randint(32,125),random.randint(32,125)))
+
+image = image.filter(ImageFilter.BLUR)
+
+image.show()
 					
