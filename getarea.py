ee675f59b6b8754c71bc910ebc36938f8a215c52
yuyi_7 <962919526@qq.com>
2017-08-27 23:39:59 +0800

    program compelet

--- getarea.py
+++ getarea.py
@@ -3,58 +3,68 @@
 
 import xlrd
 import xlwt
+import random
 
 def getin():
-	a = []
+	b = []
+	print('请输入客户订单，并以#结束输入：')
 	while True:
-		a = input('请输入客户订单号,并以#结束:')
+		a = input()
 		if a == '#':
 			break
 		a = str(a)
-	return a
+		b.append(a)
+	return b
 
 def getdata(shit):
+	exit_flag1 = len(shit)
+	get_flag = False
+	flag = 0
 	flag1 = 0
-	flag2 = 0
-	flag3 = 0
 	x = []
 	y = []
 	real = []
 	data = xlrd.open_workbook(r'C:\Users\Administrator\Desktop\shangwu.xls')
 	table = data.sheet_by_name('Sheet1')
 	table2 = data.sheet_by_name('Sheet2')
-	for i in table.nrows:
-		nrow = table.row_valuse[i]
+	for i in range(table.nrows):
+		nrow = table.row_values(i)
 
-		if nrow[0] == shit:
-			x[flag1],y[flag2] = nrow[1],nrow[2]
+		if nrow[0] in shit:
+		#	get_flag = True
+			x.append(nrow[1])
+			y.append(nrow[2])
+			flag += 1
+			for j in range(table2.nrows):
+				nrow2 = table2.row_values(j)
 
-			for j in table2.nrows:
-				nrow2 = table2.row_valuse[j]
-
-				if (x[flag1],y[flag2])==(nrow2[1],nrow2[2]):
-					real[flag3] = nrow[0]
-					flag3 += 1
-
+				if (x[flag1],y[flag1])==(nrow2[1],nrow2[2]):
+					print(nrow2[0])
+					real.append(nrow2[0])
 			flag1 += 1
-			flag2 += 1
+
+	if flag<exit_flag1:
+		print('输入错误，有数据不存在')
 
 	return real
 
 def writexle(realdata):
+	exit_flag = len(realdata)
 	flag = 0
 	wrok = xlwt.Workbook(encoding='utf-8',style_compression = 0)
 	sheet = wrok.add_sheet('sheet1',cell_overwrite_ok=True)
-	data = xlrd.open_workbook(r'C:\Users\Administrator\Desktop\shuangwu.xls')
+	data = xlrd.open_workbook(r'C:\Users\Administrator\Desktop\shangwu.xls')
 	table = data.sheet_by_name('Sheet2')
-
-	for i in table.nrows:
-		nrow = table.row_valuse[i]
-		if realdata[flag] == norw[0]:
+	for i in range(table.nrows):
+		nrow = table.row_values(i)
+		if realdata[flag] == nrow[0]:
 			x,y = nrow[1],nrow[2]
 			sheet.write(flag,0,realdata[flag])
 			sheet.write(flag,1,x)
-			sheet,write(flag,2,y)
+			sheet.write(flag,2,y)
+			flag += 1
+		if flag == exit_flag:
+				break
 
 	wrok.save(r'C:\Users\Administrator\Desktop\区域.xls')
 
