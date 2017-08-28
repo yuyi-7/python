c53d362c17d3f55d07a4ec164af19839392e31a4
Labortory <962919526@qq.com>
2017-08-28 14:17:06 +0800

    add class

--- python每天一个小程序/0013_getwedpicture.py
+++ python每天一个小程序/0013_getwedpicture.py
@@ -5,26 +5,28 @@ from bs4 import BeautifulSoup
 import os,random,sys,time
 import urllib.request
 
-def mkdirfide():
-	path = input('Please input the path:')
-	new_path = os.path.join(path,u'wed_picture')
-	if not os.path.isdir(new_path):
-		os.mkdir(new_path)
-	return new_path
+class Wedpicture(object):
+	def __init__(self,url):
+		self.url = url
 
-def page_loop(new_path):
-	
-	cont = urllib.request.urlopen(r'http://tieba.baidu.com/p/2166231880')
-	soup = BeautifulSoup(cont,'lxml')
-	myimg = soup.find_all('img',class_='BDE_Image')
-	count = 0
-	nam = range(100)
-	for i in myimg:
-		link = i.get('src')
-		pic_num = os.path.join(new_path,str(nam[count])+'.jpg')
-		count += 1
-		print(link)
-		urllib.request.urlretrieve(link,pic_num)
+	def mkdirfide(self):
+		path = input('Please input the path:')
+		new_path = os.path.join(path,u'wed_picture')
+		if not os.path.isdir(new_path):
+			os.mkdir(new_path)
+		self.new_path = new_path
+
+	def page_loop(self):
+		cont = urllib.request.urlopen(self.url)
+		soup = BeautifulSoup(cont,'lxml')
+		myimg = soup.find_all('img',class_='BDE_Image')
+		for i in myimg:
+			link = i.get('src')
+			pic_num = os.path.join(self.new_path,time.strftime('%H-%M-%S')+random.choice('qwertyuiopasdfghjklzxcvbnm')+'.jpg')
+			print(link)
+			urllib.request.urlretrieve(link,pic_num)
 
 if __name__ =='__main__':
-	page_loop(mkdirfide())
+	pa = Wedpicture('http://tieba.baidu.com/p/2166231880')
+	pa.mkdirfide()
+	pa.page_loop()
