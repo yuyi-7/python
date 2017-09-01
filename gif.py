666c38688ec0e0697341c74b2ecc281fc67016f0
yuyi_7 <962919526@qq.com>
2017-09-01 13:27:37 +0800

    a

---
+++ gif.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import requests
+import os,random
+from bs4 import BeautifulSoup
+from python.mypython import Download
+
+class gif(object):
+	def __init__(self,path):
+		self.path = path
+		if not os.path.isdir(path):
+			os.mkdir(path)
+		self.down = Download.download()
+
+	def getgif(self):
+		star_html = self.down.get('http://www.ddqsw.com/post',3)
+		soup = BeautifulSoup(star_html.text,'lxml')
+		li_lis = soup.find('div',class_='grid-gifs-wrap clearfix').find_all('a')
+		for i in li_lis:
+			title = i.get_text()
+			href = i.get('href')
+			html = self.down.get(href,3)
+			html_soup = BeautifulSoup(html.text,'lxml')
+			page = html_soup.find('div',class_='btn-group clearfix full-width pagination-block').find_all('a')[-1].get_text()
+			for p in range(page):
+				page_url = href[0:-5]-+'/'+str(p)+'.html'
+				img_html = self.down.get(page_url,3)
+				img_soup = BeautifulSoup(img_html.text,'lxml')
+				img_url = img_soup.find('div',class_='adetail').find_all('img')
+				for m in img_url:
+					myimg = m.get('src')
+					dimg = self.down.get(myimg,3)
+					print('抓取%s，开始保存'%(myimg))
+					f = open(self.path+'\\'+myimg[-9:],'ab')
+					f.write(dimg.content)
+					f.close()
+pa = input('保存路径：')
+man = gif(pa)
+man.getgif()
