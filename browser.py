10021e23632e83906371ff1c8ed108859fcac65d
Labortory <962919526@qq.com>
2017-09-03 17:14:29 +0800

    from elenium import webdriver

---
+++ browser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+print(driver.page_source.encode('utf-8'))
